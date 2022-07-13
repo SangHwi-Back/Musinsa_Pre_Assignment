cb873f235f504279ce12703c21d6d8d3161c75d7
백상휘 <sanghwiback@gmail.com>
2022-07-13 10:44:04 +0900

    Initial Commit

---
+++ MusinsaBeck/MusinsaBeck/SceneDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SceneDelegate.swift
+//  MusinsaBeck
+//
+//  Created by 백상휘 on 2022/07/13.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+}
---
+++ MusinsaBeck/MusinsaBeck/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  MusinsaBeck
+//
+//  Created by 백상휘 on 2022/07/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
