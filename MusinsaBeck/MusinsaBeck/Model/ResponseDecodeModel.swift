a0c1f4b6bf7a1cdbbd32de214dbb08e31e09331e
백상휘 <sanghwiback@gmail.com>
2022-07-14 09:51:49 +0900

    Logging decode json

--- MusinsaBeck/MusinsaBeck/Model/ResponseDecodeModel.swift
+++ MusinsaBeck/MusinsaBeck/Model/ResponseDecodeModel.swift
@@ -6,30 +6,37 @@
 //
 
 import Foundation
+import os.log
 
 class ResponseDecodeModel<T: Decodable>: Disposable {
     
     private let decoder = JSONDecoder()
     
     func decode(_ data: Data) -> T? {
-        
         do {
             return try decoder.decode(T.self, from: data)
-        } catch DecodingError.dataCorrupted(let context) {
-            print(context)
-        } catch DecodingError.keyNotFound(let key, let context) {
-            print("Key '\(key)' not found:", context.debugDescription)
-            print("codingPath:", context.codingPath)
-        } catch DecodingError.valueNotFound(let value, let context) {
-            print("Value '\(value)' not found:", context.debugDescription)
-            print("codingPath:", context.codingPath)
-        } catch DecodingError.typeMismatch(let type, let context) {
-            print("Type '\(type)' mismatch:", context.debugDescription)
-            print("codingPath:", context.codingPath)
         } catch {
-            print("error: ", error)
+            logError(error)
         }
-        
         return nil
+    }
+    
+    private func logError(_ error: Error) {
+        
+        let logger = Logger()
+        
+        switch error {
+        case DecodingError.dataCorrupted(let context):
+            logger.log(level: .debug, "[ResponseDecodeError] dataCorrupted.\n\(context.debugDescription)")
+        case DecodingError.keyNotFound(let key, let context):
+            logger.log(level: .info, "[ResponseDecodeError] keyNotFound.\nKey : \(key.stringValue), Description : \(context.debugDescription)")
+        case DecodingError.valueNotFound(let value, let context):
+            logger.log(level: .info, "[ResponseDecodeError] valueNotFound.\nValue : \(value), Description : \(context.debugDescription)")
+        case DecodingError.typeMismatch(let type, let context):
+            logger.log(level: .info, "[ResponseDecodeError] typeMismatch.\nType : \(type), Description : \(context.debugDescription)")
+        default:
+            logger.log(level: .debug, "\(error.localizedDescription)")
+            return
+        }
     }
 }
